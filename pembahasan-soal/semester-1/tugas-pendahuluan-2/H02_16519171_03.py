d26488f5b356973250e670d47379b25004fcf5f2
mgstabrani <mgstabraniitb@gmail.com>
2020-07-12 22:59:54 +0700

    Modifying H02_16519171_03.py

--- pembahasan-soal/semester-1/tugas-pendahuluan-2/H02_16519171_03.py
+++ pembahasan-soal/semester-1/tugas-pendahuluan-2/H02_16519171_03.py
@@ -1,29 +1,20 @@
-# NIM/Nama : 16519171/Mgs. Tabrani
-# Tanggal : 8 Oktober 2019
-# Deskripsi : Program_Bilangan_Prima
-#Menentukan_bahwa_Masukan_Bilangan_Prima_atau_Bukan
+'''This program will determine weather a number is prime or not'''
 
-#Kamus
-#X, i : int
-#stop : boolean
-
-#Algoritma
-X = int(input("Masukkan X: "))
-i = 2
-stop = False
-
-if (X < 2):
-    print(str(X) + " bukan bilangan prima") #Jika X kurang dari 2 bukan bilangan prima
-
+X = int(input("Input X: "))
+prime = True
+if (X<2) :
+    prime = False
 else:
     for i in range(2,X):
-        if (X%i == 0):
-            stop = x
-        else :
-            i = i+1
-    if (stop == True) :
-        print(str(X) + " bukan bilangan prima")
-    else:
-        print(str(X) + " adalah bilangan prima")
+        if (X % i == 0):
+            prime = False
+
+if (prime==True):
+    print(X, "is prime number")
+else:
+    print(X, "is not prime number")
+
+
+
 
     
